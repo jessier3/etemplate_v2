519d4ac42c424da68a5212226a011ac1e7f1c518
Jessica Rawlins <jrawlins@1800contacts.com>
2021-11-26 11:31:42 -0700

    Authorize Testing

---
+++ tests/Unit/PaymentTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\;
+
+class ExampleTest extends TestCase
+{
+    public function test_authorize_transaction_sandbox()
+    {
+        $total = rand(1, 100) + rand(0,100)/100;
+        $auth_trans_id = FALSE;
+
+    }
+}
